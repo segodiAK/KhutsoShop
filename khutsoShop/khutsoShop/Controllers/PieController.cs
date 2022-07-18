35434e101c72f974b4c50a3887130173c1839c56
KHUTSO SEGODI <77273156+segodiAK@users.noreply.github.com>
2022-07-18 11:00:45 +0200

    Add files via upload

---
+++ khutsoShop/khutsoShop/Controllers/PieController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using khutsoShop.Models;
+using khutsoShop.ViewModels;
+//using khutsoShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace khutsoShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+        private object piesListViewModel;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public ViewResult List()
+        {
+            PiesListViewModel piesListViewModel = new PiesListViewModel();
+            piesListViewModel.Pies = _pieRepository.AllPies;
+
+            piesListViewModel.CurrentCategory = "Banana cakes";
+            return View(piesListViewModel);
+        }
+    }
+}
